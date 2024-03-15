683ce1f958acf89d548bd60f2f82e9f63441d8d7
Eric Petterson <ericpl.ff@gmail.com>
2024-03-14 23:07:10 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Jornada de dados")
+
+# Variaveis
+idade = 36
+nome = "Eric"
+
+# Mostrar
+print(idade)
+
+# Soma
+nova_idade = idade + 3
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+print(lista)
+
+# Métodos
+lista_inv = lista.reverse()
+print(lista_inv)
+
